cef0b0c425aefbf5a74983b80b38b8c366cb571f
TrungDC0708 <hihi19111@gmail.com>
2023-04-02 17:17:17 +0700

    tim duong

--- findWay.py
+++ findWay.py
@@ -1,25 +1,13 @@
 import pandas as pd
-from utils import Euclidean
+from utils import *
 import ast
+import algorithm as al
+from tqdm import tqdm
+import redis
 
-map = pd.read_excel("map350_040122_X.xlsx")
-nodes = []
-d = dict()
-for i in range(len(map.index)):
-    for j in range(len(map.columns)):
-        temp = map.iloc[i][j].split(",")
-        if temp[0] == 'x':
-            if map.iloc[i - 1][j].split(",")[0] != "q":
-                nodes.append([i - 1, j, map.iloc[i - 1][j].split(",")])
-            if map.iloc[i + 1][j].split(",")[0] != "q":
-                nodes.append([i + 1, j, map.iloc[i + 1][j].split(",")])
-            if map.iloc[i][j - 1].split(",")[0] != "q":
-                nodes.append([i, j - 1, map.iloc[i][j - 1].split(",")])
-            if map.iloc[i][j + 1].split(",")[0] != "q":
-                nodes.append([i, j + 1, map.iloc[i][j + 1].split(",")])
-        if len(temp) >= 2:
-            nodes.append([i, j, temp])
-
+r = redis.Redis(db=4)
+r2 = redis.Redis(db=3)
+r3 = redis.Redis(db=5)
 """gen undirected graph"""
 for node in nodes:
     minXleftOfNode = 100000
@@ -76,10 +64,31 @@ for node1 in d:
             if node1_change[0] - node2[0] > 0:
                 dir = 'u'
         if dir in temp:
-            list_node.append([node2[1], node2[0],Euclidean(node1_change[1], node1_change[0], node2[1], node2[0])])
+            list_node.append([node2[1], node2[0], Euclidean(node1_change[1], node1_change[0], node2[1], node2[0])])
 
     vertexes[str([node1_change[1], node1_change[0]])] = list_node
 
-for i in vertexes:
-    print(i + " " + str(vertexes[i]))
+graph1 = al.Graph(vertexes)
+for i in tqdm(outport):
+    min = 100000
+    shortest_inport = i.copy()
+    for j in inport:
+        distance = Euclidean(i[0], i[1], j[0], j[1])
+        if distance < min:
+            min = distance
+            shortest_inport = j.copy()
+    r2.set(str(i[0]) + "," + str(i[1]), str(shortest_inport[0]) + ',' + str(shortest_inport[1]))
 
+
+for i in tqdm(outport):
+    for j in inport:
+        route = graph1.a_star_algorithm(str(i), str(j))
+        if route is not None:
+            r3.set(str(i[0]) + "," + str(i[1]) + ',' + str(j[0]) + ',' + str(j[1]), str(route))
+
+
+for i in tqdm(inport):
+    for j in outport:
+        route = graph1.a_star_algorithm(str(i), str(j))
+        if route is not None:
+            r.set(str(i[0]) + "," + str(i[1]) + ',' + str(j[0]) + ',' + str(j[1]), str(route))
